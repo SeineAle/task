36aee0faa9b1519cce76c511c9ead170434a4a06
Paras Jain <paras.jain.real@gmail.com>
2024-06-23 10:41:07 +0530

    Update Card.jsx

--- shopping-site/src/components/Card.jsx
+++ shopping-site/src/components/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ id, items }) => {
-    return (
-      <div className="card border border-gray-300 shadow-md rounded-md p-4 bg-gray-100">
+    return (  
+    <div className="card border border-gray-300 shadow-md rounded-md p-4 bg-gray-100 my-4 mx-4">
         <div className="flex justify-between items-center">
           <div className="flex-1">
             <p className="text-lg font-semibold text-gray-800">{items.first.item} - {items.first.price} INR</p>
